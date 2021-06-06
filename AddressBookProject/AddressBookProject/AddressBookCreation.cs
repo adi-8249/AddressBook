8b013e910b2756dc5ced09c8e5c867ba3fbf44e6
Anjaneya <anjaneyaprasad9437@gmail.com>
2021-06-06 22:16:51 +0530

    [Anjaneya]Added:Displaying the informations of person

---
+++ AddressBookProject/AddressBookProject/AddressBookCreation.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookProject
+{
+
+    public class AddressBookCreation
+    {
+        private string firstName;
+        private string lastName;
+        private string address;
+        private string city;
+        private string state;
+        private long mobileNo;
+        private int zip;
+
+        public AddressBookCreation(string firstName, string lastName, string address, string city, string state, long mobileNo, int zip)
+        {
+
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.mobileNo = mobileNo;
+            this.zip = zip;
+        }
+        public string FirstName 
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+        public string LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+        public string Address
+        {
+            get { return address; }
+            set { address = value; }
+        }
+        public string City
+        {
+            get { return city; }
+            set { city = value; }
+        }
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+        public long MobileNo
+        {
+            get { return mobileNo; }
+            set { mobileNo = value; }
+        }
+        public int Zip
+        {
+            get { return zip; }
+            set { zip = value; }
+        }
+
+
+
+
+
+
+
+
+    }
+}
--- AddressBookProject/AddressBookProject/Program.cs
+++ AddressBookProject/AddressBookProject/Program.cs
@@ -6,7 +6,17 @@ namespace AddressBookProject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Address book program");
+            AddressBookCreation creation = new AddressBookCreation("Anjaneya", "Tosh", "Nuapatna", "Cuttack", "Odisha", 9456745454, 754035);
+            Console.WriteLine("First Name is:"+ creation.FirstName);
+            Console.WriteLine("Last Name is:"+ creation.LastName);
+            Console.WriteLine("Address is:"+ creation.Address);
+            Console.WriteLine("City is:"+ creation.City);
+            Console.WriteLine("State is:"+ creation.State);
+            Console.WriteLine("Mobile number is:"+ creation.MobileNo);
+            Console.WriteLine("Zip code is:"+ creation.Zip);
+
+
+
         }
     }
 }
